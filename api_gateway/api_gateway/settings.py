3cd3d3753d55520a275722b9cf74a2c104af8bb6
Vance Tindoc <149294631+Vanz1233@users.noreply.github.com>
2026-04-26 02:17:57 +0800

    Fix: Added wildcard to ALLOWED_HOSTS for Render

--- api_gateway/api_gateway/settings.py
+++ api_gateway/api_gateway/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-local-dev')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
